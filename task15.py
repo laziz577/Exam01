be9b14699be358ea333a7a41207cb294c0e6d876
laziz577 <temirovabdulaziz042@gmail.com>
2025-06-27 18:45:34 +0500

    bajarildi

---
+++ task15.py
@@ -0,0 +1,10 @@
+number = int(input("Boshlanish:"))
+print("Tugashi")
+
+if number % 2 == 0:
+   print(f"{number} soni 2 ga bolinadi")
+if number % 2 == 0:
+     print(f"{number} soni 3 ga bolinadi")  
+if number % 2 == 0:
+    print(f"{number} soni 5 ga bolinadi") 
+     
