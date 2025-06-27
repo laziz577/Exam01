16b3c1662fc15cc29bc8c82725f334ce95868a6b
laziz577 <temirovabdulaziz042@gmail.com>
2025-06-27 19:27:42 +0500

    bajarildi

---
+++ task17.py
@@ -0,0 +1,20 @@
+bal = int(input("balni kiriting:"))
+
+if bal >=90 and bal <= 100:
+    print("A(Alo)")
+
+if bal >=80 and bal <=89:
+    print("B(Yaxshi)")
+
+if bal >= 70 and bal <= 79:
+    print("C(Qoniqarli)")
+
+if bal >=60 and bal <= 69:
+    print("D(QOniqarsiz)")
+
+if bal >= 0 and bal <= 59:
+    print("F(Yomon)")
+else:
+    print("Ball 0-100 oralig'ida bulishi kerak")
+
+
